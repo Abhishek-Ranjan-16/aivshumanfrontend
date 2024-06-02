7b1530abf8db1233db0aeba7a600f049483866cd
AssassinOFsilchar <abhiran14082002@gmail.com>
2024-06-02 21:00:00 +0530

    changed the backend url

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
         throw new Error("Text is not defined");
       }
 
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://aivshumanbackend-1.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
